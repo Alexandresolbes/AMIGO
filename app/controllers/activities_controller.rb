f0f85782af430e2a24b7c6d4b724195f3b010161
alexandre solbes <alexandresolbes@gmail.com>
2022-08-29 17:05:29 +0200

    integrating geocoder

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -6,59 +6,66 @@ class ActivitiesController < ApplicationController
   def index
     @activities = policy_scope(Activity)
     @activities = current_user.activities
+    @markers = @activities.geocoded.map do |activity|
+      {
+      lat: activity.latitude,
+      lng: activity.longitude,
+      info_window: render_to_string(partial: "info_window", locals: {activity: activity})
+    }
   end
+end
 
-  def show
-    authorize @activity
+def show
+  authorize @activity
+end
+
+def new
+  @activity = Activity.new
+  authorize @activity
+end
+
+def create
+  @activity = Activity.new(activity_params)
+  @activity.trip = @trip
+  @activity.user = current_user
+  authorize @activity
+  if @activity.save!
+    redirect_to @activity, notice: "Activity created !"
+  else
+    render :new, status: :unprocessable_entity
   end
+end
 
-  def new
-    @activity = Activity.new
-    authorize @activity
+def edit
+  authorize @activity
+end
+
+def update
+  authorize @activity
+  if @activity.update(activity_params)
+    redirect_to @activity, notice: "Activity updated successfully."
+  else
+    render :edit, status: :unprocessable_entity
   end
+end
 
-  def create
-    @activity = Activity.new(activity_params)
-    @activity.trip = @trip
-    @activity.user = current_user
-    authorize @activity
-    if @activity.save!
-      redirect_to @activity, notice: "Activity created !"
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
+def destroy
+  authorize @activity
+  @activity.destroy
+  redirect_to activities_path, status: :see_other
+end
 
-  def edit
-    authorize @activity
-  end
+private
 
-  def update
-    authorize @activity
-    if @activity.update(activity_params)
-      redirect_to @activity, notice: "Activity updated successfully."
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
+def activity_params
+  params.require(:activity).permit(:categories, :address, :description, :min_amigos, :max_amigos, :title, :date)
+end
 
-  def destroy
-    authorize @activity
-    @activity.destroy
-    redirect_to activities_path, status: :see_other
-  end
+def set_activity
+  @activity = Activity.find(params[:id])
+end
 
-  private
-
-  def activity_params
-    params.require(:activity).permit(:categories, :address, :description, :min_amigos, :max_amigos, :title, :date)
-  end
-
-  def set_activity
-    @activity = Activity.find(params[:id])
-  end
-
-  def set_trip
-    @trip = Trip.find(params[:trip_id])
-  end
+def set_trip
+  @trip = Trip.find(params[:trip_id])
+end
 end

7214bde94484e6a18c68e3517247efe476000ff0
Isabel Lamim <lamim@protonmail.com>
2022-08-29 17:36:51 +0200

    fixed pundit and controller issues

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,6 @@
 class Activity < ApplicationRecord
   belongs_to :trip
-  has_many :users, through: :particaptions
+  has_many :users, through: :participations
 
   has_one_attached :photo
   geocoded_by :address

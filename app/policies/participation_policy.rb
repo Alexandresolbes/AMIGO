eac1dba88d6102a2487450c6d93023e85faac17b
Isabel Lamim <lamim@protonmail.com>
2022-08-29 16:09:29 +0200

    added pundit auth to participations, activities and user trips

---
+++ app/policies/participation_policy.rb
@@ -0,0 +1,16 @@
+class ParticipationPolicy < ApplicationPolicy
+  def create?
+    true
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+end

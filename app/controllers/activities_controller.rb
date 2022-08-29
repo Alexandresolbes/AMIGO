742d16f36463dcf36a6a0b758af7cb88c1333bf5
Isabel Lamim <lamim@protonmail.com>
2022-08-29 17:17:52 +0200

    fixed indentation after conflict

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -11,34 +11,34 @@ class ActivitiesController < ApplicationController
       lng: activity.longitude,
       info_window: render_to_string(partial: "info_window", locals: {activity: activity})
     }
+    end
   end
-end
 
-def show
-  authorize @activity
-end
-
-def new
-  @activity = Activity.new
-  authorize @activity
-end
-
-def create
-  @activity = Activity.new(activity_params)
-  @activity.trip = @trip
-  @activity.user = current_user
-  authorize @activity
-  if @activity.save!
-    redirect_to @activity, notice: "Activity created !"
-  else
-    render :new, status: :unprocessable_entity
+  def show
+    authorize @activity
   end
-end
+
+  def new
+    @activity = Activity.new
+    authorize @activity
+  end
+
+  def create
+    @activity = Activity.new(activity_params)
+    @activity.trip = @trip
+    @activity.user = current_user
+    authorize @activity
+    if @activity.save!
+      redirect_to @activity, notice: "Activity created !"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   def edit
   end
 
-def update
+  def update
     if @activity.update(activity_params)
       redirect_to @activity, notice: "Activity updated successfully."
     else
@@ -46,23 +46,25 @@ def update
     end
   end
 
-def destroy
-  authorize @activity
-  @activity.destroy
-  redirect_to activities_path, status: :see_other
+  def destroy
+    authorize @activity
+    @activity.destroy
+    redirect_to activities_path, status: :see_other
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:categories, :address, :description, :min_amigos, :max_amigos, :title, :date)
+  end
+
+  def set_activity
+    @activity = Activity.find(params[:id])
+  end
+
+  def set_trip
+    @trip = Trip.find(params[:trip_id])
+  end
 end
 
-private
-
-def activity_params
-  params.require(:activity).permit(:categories, :address, :description, :min_amigos, :max_amigos, :title, :date)
-end
-
-def set_activity
-  @activity = Activity.find(params[:id])
-end
-
-def set_trip
-  @trip = Trip.find(params[:trip_id])
-end
-end
+# commentaire

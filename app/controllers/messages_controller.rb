108ff06804351431f8c5be321a6b737809930b68
Isabel Lamim <lamim@protonmail.com>
2022-08-30 11:46:47 +0200

    fixed pundit and redirection issues

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,13 @@
 class MessagesController < ApplicationController
   def create
     @room = Room.find(params[:room_id])
+    trip = Trip.find(params[:trip_id])
     @message = Message.new(message_params)
     @message.room = @room
     @message.user = current_user
+    authorize @message
     if @message.save
-      redirect_to trip_room_path(@room)
+      redirect_to trip_room_path(trip, @room)
     else
       render "rooms/show", status: :unprocessable_entity
     end
@@ -16,5 +18,4 @@ class MessagesController < ApplicationController
   def message_params
     params.require(:message).permit(:content)
   end
-end
 end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,6 +2,7 @@ class RoomsController < ApplicationController
   def show
     @trip = Trip.find(params[:trip_id])
     @room = Room.find(params[:id])
+    authorize @room
     @message = Message.new
   end
 end

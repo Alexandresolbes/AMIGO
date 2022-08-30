4766b4dea497c4e030317c640735a0030c1d8824
Isabel Lamim <lamim@protonmail.com>
2022-08-30 12:11:20 +0200

    added dependencies to models

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,5 @@
 class Trip < ApplicationRecord
-  has_many :activities
-  has_many :user_trips
+  has_many :activities, dependent: :destroy
+  has_many :user_trips, dependent: :destroy
   has_many :users, through: :user_trips
 end

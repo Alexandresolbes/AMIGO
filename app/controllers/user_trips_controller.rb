c5ac7dcb629d3ccfc056b084aa54f1717aa9cc67
Isabel Lamim <lamim@protonmail.com>
2022-09-01 14:43:06 +0200

    added trip adhesion/removal buttons

--- app/controllers/user_trips_controller.rb
+++ app/controllers/user_trips_controller.rb
@@ -2,16 +2,19 @@ class UserTripsController < ApplicationController
   def create
     @user_trip = UserTrip.new
     @user_trip.user = current_user
-    @user_trip.trip = params[:trip_id]
+    @user_trip.trip_id = params[:trip_id]
+    authorize @user_trip
     if @user_trip.save!
-      redirect_to trips_path, notice: "You participate in this trip."
+      redirect_to trip_path(@user_trip.trip_id), notice: "You participate in this trip."
     else
       redirect_to trips_path, notice: "Error! Please try again."
     end
   end
+
   def destroy
+    @user_trip = UserTrip.find(params[:user_trip])
     authorize @user_trip
     @user_trip.destroy
-    redirect_to trips_path, status: :see_other
+    redirect_to trip_path(@user_trip.trip_id), status: :see_other
   end
 end

8f18402a767e3b917648e0ffbfaffa6d9b25a694
Isabel Lamim <lamim@protonmail.com>
2022-09-01 16:45:03 +0200

    added user trips to notifications

--- app/controllers/user_trips_controller.rb
+++ app/controllers/user_trips_controller.rb
@@ -6,6 +6,7 @@ class UserTripsController < ApplicationController
     authorize @user_trip
     if @user_trip.save!
       redirect_to trip_path(@user_trip.trip_id), notice: "You participate in this trip."
+      create_notification("joined", @user_trip)
     else
       redirect_to trips_path, notice: "Error! Please try again."
     end
@@ -16,5 +17,16 @@ class UserTripsController < ApplicationController
     authorize @user_trip
     @user_trip.destroy
     redirect_to trip_path(@user_trip.trip_id), status: :see_other
+  end
+
+  private
+
+  def create_notification(action, target)
+    @user = current_user
+    @trip = Trip.find(target.trip_id)
+    @notification = Notification.new(content: "#{@trip.destination} trip was #{action}")
+    @notification.user_id = current_user.id
+    @notification.trip_id = @trip.id
+    @notification.save!
   end
 end

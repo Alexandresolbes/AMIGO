ab3994cda6ec3f91cceab77b62d5039b9f81e0a8
Laura Latuillerie <laura.latuillerie@gmail.com>
2022-09-05 16:29:57 +0200

    All working only for wallet owner who created the bills. Accounts with each user and balances working

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -9,9 +9,7 @@ class BillsController < ApplicationController
     authorize @bill
     @bill.wallet_id = @wallet.id
     @bill.paid = false
-    @bill.wallet_id = @wallet.id
     if @bill.save!
-      amigo_bill(@bill).save!
       redirect_to trip_wallet_path(@trip, @wallet), notice: "Bill created!"
     else
       render :new, status: :unprocessable_entity
@@ -28,19 +26,6 @@ class BillsController < ApplicationController
   end
 
   private
-
-  def amigo_bill(bill)
-    @amigo_bill = Bill.new(bill_params)
-    
-    @amigo_bill.debit = bill.credit if !bill.debit
-    @amigo_bill.credit = bill.debit if !bill.credit
-
-    @amigo_bill.wallet_id = bill.user_id
-
-    @amigo_bill.user_id = bill.wallet_id
-
-    @amigo_bill
-  end
 
   def bill_params
     params.require(:bill).permit(:debit, :credit, :paid, :user_id)
